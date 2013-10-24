ffd78418a830e03dcac45844115810c69fdbcb80
Michael Kuenzli <michaelkuenzli@gmail.com>
2013-10-24 12:51:53 +0200

    changed to buzzer example

--- ACSTest/Program.cs
+++ ACSTest/Program.cs
@@ -10,26 +10,55 @@ namespace ACSTest
 {
     class Program
     {
-
-       
+        static List<Answer> answers = new List<Answer>();
         
-
         static void Main(string[] args)
         {
-
             PCSCReader r = new PCSCReader();
             r.NewNdefMessageDetected += r_NewNdefMessageDetected;
+            Console.WriteLine("Tag mit NDEF-Message auf Leser legen, um Buzzer zu initialisieren");
             Console.ReadLine();
             r.NewNdefMessageDetected -= r_NewNdefMessageDetected;
-
-
         }
 
         static void r_NewNdefMessageDetected(NdefMessage message, int readerId)
         {
-            Console.WriteLine("NdefMessage at reader {0}", readerId);
+            Answer a = answers.Find(an => an.ReaderID == readerId);
+            if (a == null)
+            {
+                a = new Answer(readerId);
+                answers.Add(a);
+                Console.WriteLine("Buzzer '{0}' initialisiert", a.Label);
+            }
+            var textRecord = new NdefTextRecord(message.First());
+
+            Console.WriteLine("Tag '{0}' an Buzzer '{1}'", textRecord.Text, a.Label);
         }
 
 
+    }
+
+    public class Answer
+    {
+        private static int i = 0;
+        static String[] labels = new String[] { "Eins", "Zwei", "Drei", "Vier", "Fünf" };
+
+        private String _label;
+        public String Label
+        {
+            get { return _label; }
+        }
+
+        private int _readerId;
+        public int ReaderID
+        {
+            get { return _readerId; }
+        }
+
+        public Answer(int readerId)
+        {
+            _readerId = readerId;
+            _label = labels[i++];
+        }
     }
 }
